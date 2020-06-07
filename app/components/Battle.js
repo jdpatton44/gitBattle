12d8ddf9b719340a425b94d503ab1fd17a26d7c4
JD <jdpattonv@gmail.com>
2020-06-06 23:39:43 -0400

    added api calls

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
+import { FaUserFriends, FaTimesCircle, FaFighterJet, FaTrophy } from 'react-icons/fa';
 import PropTypes from 'prop-types';
+import Results from './Results';
 
 function Instructions() {
   return (
@@ -77,13 +78,47 @@ PlayerInput.propTypes = {
   label: PropTypes.string.isRequired,
 };
 
+function PlayerPreview({ username, onReset, label }) {
+  return (
+    <div className="column player">
+      <h3 className="player-label">{label}</h3>
+      <div className="row bg-light">
+        <div className="player-info">
+          <img
+            className="avatar-small"
+            src={`https://github.com/${username}.png?size200`}
+            alt={`Avatar for ${username}`}
+          />
+          <a href={`https://github.com/${username}`} className="link">
+            {username}
+          </a>
+        </div>
+        <button className="btn-clear flex-center" onClick={onReset}>
+          <FaTimesCircle color="rgb(194,57,42)" size={26} />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+PlayerPreview.propTypes = {
+  username: PropTypes.string.isRequired,
+  onReset: PropTypes.func.isRequired,
+  label: PropTypes.string.isRequired,
+};
+
 export default class Battle extends React.Component {
   constructor(props) {
     super(props);
+
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
   }
 
   handleSubmit(id, player) {
@@ -92,8 +127,18 @@ export default class Battle extends React.Component {
     });
   }
 
+  handleReset(id) {
+    this.setState({
+      [id]: null,
+    });
+  }
+
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
 
     return (
       <React.Fragment>
@@ -101,14 +146,24 @@ export default class Battle extends React.Component {
         <div className="players-container">
           <h1 className="center-text header-lg">Players</h1>
           <div className="row space-around">
-            {playerOne === null && (
+            {playerOne === null ? (
               <PlayerInput label="Player One" onSubmit={player => this.handleSubmit('playerOne', player)} />
+            ) : (
+              <PlayerPreview username={playerOne} label="Player One" onReset={() => this.handleReset('playerOne')} />
             )}
 
-            {playerTwo === null && (
-              <PlayerInput label="Player One" onSubmit={player => this.handleSubmit('playerOne', player)} />
+            {playerTwo === null ? (
+              <PlayerInput label="Player Two" onSubmit={player => this.handleSubmit('playerTwo', player)} />
+            ) : (
+              <PlayerPreview username={playerTwo} label="Player Two" onReset={() => this.handleReset('playerTwo')} />
             )}
           </div>
+
+          {playerOne && playerTwo && (
+            <button className="btn dark-btn btn-space" onClick={this.setState({ battle: true })}>
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );
