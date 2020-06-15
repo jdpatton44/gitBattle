eeed22e1c27f8d1d09badaa4346f66085229a79c
JD <jdpattonv@gmail.com>
2020-06-14 23:21:08 -0400

    added tooltip component

--- app/components/popular.js
+++ app/components/popular.js
@@ -5,6 +5,7 @@ import { FaUser, FaStar, FaCodeBranch, FaExclamationTriangle } from 'react-icons
 import { fetchPopularRepos } from '../utils/api';
 import Card from './Card';
 import Loading from './Loading';
+import Tooltip from './Tooltip';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -41,8 +42,10 @@ function ReposGrid({ repos }) {
             <Card header={`#${index + 1}`} avatar={avatar_url} href={html_url} name={login}>
               <ul className="card-list">
                 <li>
-                  <FaUser color="rgb(255,191,116)" size={22} />
-                  <a href={`https://github.com/${login}`}>{login}</a>
+                  <Tooltip text="Github User Name">
+                    <FaUser color="rgb(255,191,116)" size={22} />
+                    <a href={`https://github.com/${login}`}>{login}</a>
+                  </Tooltip>
                 </li>
                 <li>
                   <FaStar color="rgb(255,215,9)" size={22} />
