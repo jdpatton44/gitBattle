623152aa8c86b2686cac6c7df47113910a6b3bf0
JD <jdpattonv@gmail.com>
2020-07-13 22:36:20 -0400

    converted Card and Nav to functional components
    converted hovering to useHover hook

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,33 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { ThemeConsumer } from '../contexts/theme';
+import ThemeContext from '../contexts/theme';
 
 const activeStyle = {
   color: 'rgb(187, 46, 31)',
 };
 
 export default function Nav({ toggleTheme }) {
+  const theme = React.useContext(ThemeContext);
+
   return (
-    <ThemeConsumer>
-      {theme => (
-        <nav className="row space-between">
-          <ul className="row nav">
-            <li>
-              <NavLink to="/" activeStyle={activeStyle} exact className="nav-link">
-                Popular
-              </NavLink>
-            </li>
-            <li>
-              <NavLink to="/battle" activeStyle={activeStyle} exact className="nav-link">
-                Battle
-              </NavLink>
-            </li>
-          </ul>
-          <button style={{ fontSize: 30 }} className="btn-clear" onClick={toggleTheme} type="button">
-            {theme === 'light' ? '🔦' : '💡'}
-          </button>
-        </nav>
-      )}
-    </ThemeConsumer>
+    <nav className="row space-between">
+      <ul className="row nav">
+        <li>
+          <NavLink to="/" activeStyle={activeStyle} exact className="nav-link">
+            Popular
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/battle" activeStyle={activeStyle} exact className="nav-link">
+            Battle
+          </NavLink>
+        </li>
+      </ul>
+      <button style={{ fontSize: 30 }} className="btn-clear" onClick={toggleTheme} type="button">
+        {theme === 'light' ? '🔦' : '💡'}
+      </button>
+    </nav>
   );
 }
